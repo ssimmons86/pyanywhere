565ea25743cb972a4485b9c90407dfbb1504a0f0
ssimmons86 <126532434+ssimmons86@users.noreply.github.com>
2023-04-28 23:53:54 -0500

    Update settings.py

--- Group7Collect/settings.py
+++ Group7Collect/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&-obju4p+)dz0z=xe*3blj8o9s9g@7aa@d1*x^z_@ez_t2=ctq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [['127.0.0.1', '.pythonanywhere.com']]
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
